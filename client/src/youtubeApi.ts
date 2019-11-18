2d09d4068644b964a244c392d6e691452a02a33d
David Harush <davidharush@gmail.com>
2019-11-18 12:40:35 +0200

    youtube key removed

--- client/src/youtubeApi.ts
+++ client/src/youtubeApi.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const KEY = 'AIzaSyBjvnxGPUPE7wV0seQZTF8GO17pAyCcaLU';
+const KEY = '';
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3/',
